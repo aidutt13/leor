ca81b7c9b28c6d7635d06762cd9cac2a832cdec4
Wojciech Surdyk <aidutt2115@gmail.com>
2022-05-09 15:15:58 +0200

    New AST implementation dropped derived classes in favour of data classes

--- src/Parser/AST.cpp
+++ src/Parser/AST.cpp
@@ -1,12 +1,166 @@
 #include "Parser/AST.h"
 
-namespace leor
+namespace leor_new
+{
+  using namespace std::string_literals;
+
+  AST& AST::set(Type type)
+  {
+    this->type = type;
+    return *this;
+  }
+
+  AST& AST::set(const std::tuple<uint64_t, uint64_t>& pos)
+  {
+    this->pos = pos;
+    return *this;
+  }
+
+  AST& AST::set(const std::string& key, const Value& value)
+  {
+    values.insert_or_assign(key, value);
+    return *this;
+  }
+
+  AST& AST::set(const std::string& key, const AST& value)
+  {
+    values.insert_or_assign(key, value);
+    return *this;
+  }
+
+  AST AST::None()
+  {
+    return AST().set(AST::Type::NONE);
+  }
+
+  AST AST::Bool(const bool& value, const std::tuple<uint64_t, uint64_t> pos)
+  {
+    return AST().set(AST::Type::BOOL).set(pos).set("value", value);
+  }
+
+  AST AST::Int(const int64_t& value, const std::tuple<uint64_t, uint64_t> pos)
+  {
+    return AST().set(AST::Type::INT).set(pos).set("value", value);
+  }
+
+  AST AST::Float(const double& value, const std::tuple<uint64_t, uint64_t> pos)
+  {
+    return AST().set(AST::Type::FLOAT).set(pos).set("value", value);
+  }
+
+  AST AST::String(const std::string& value, const std::tuple<uint64_t, uint64_t> pos)
+  {
+    return AST().set(AST::Type::STRING).set(pos).set("value", value);
+  }
+
+  AST AST::Char(const char& value, const std::tuple<uint64_t, uint64_t> pos)
+  {
+    return AST().set(AST::Type::CHAR).set(pos).set("value", value);
+  }
+
+  AST AST::Var(const std::string& value, const std::tuple<uint64_t, uint64_t> pos)
+  {
+    return AST().set(AST::Type::VAR).set(pos).set("value", value);
+  }
+
+  AST AST::Function(
+    const std::string& name,
+    const std::vector<AST>& args,
+    const AST& body,
+    const std::tuple<uint64_t, uint64_t> pos = std::make_tuple(0UL, 0UL)
+  ) {
+    
+    return AST()
+      .set(AST::Type::FUNCTION)
+      .set(pos)
+      .set("name", name)
+      .set("args", args)
+      .set("body", body);
+  }
+
+  AST AST::Variable(
+    const std::string& name,
+    const AST& value,
+    const bool& is_constant,
+    const std::tuple<uint64_t, uint64_t> pos
+  ) {
+
+    return AST()
+      .set(AST::Type::VARIABLE)
+      .set(pos)
+      .set("name", name)
+      .set("value", value)
+      .set("is_constant", is_constant);
+  }
+
+  AST AST::Call(
+    AST function,
+    const std::vector<AST>& args,
+    const std::tuple<uint64_t, uint64_t> pos
+  ) {
+
+    return AST()
+      .set(AST::Type::CALL)
+      .set(pos)
+      .set("function", function)
+      .set("args", args);
+  }
+
+  AST AST::Binary(
+    const std::string& op,
+    const AST& left,
+    const AST& right,
+    const std::tuple<uint64_t, uint64_t> pos
+  ) {
+
+    return AST()
+      .set(AST::Type::BINARY)
+      .set(pos)
+      .set("op", op)
+      .set("left", left)
+      .set("right", right);
+  }
+
+  AST AST::Assign(
+    const std::string& op,
+    const AST& left,
+    const AST& right,
+    const std::tuple<uint64_t, uint64_t> pos
+  ) {
+
+    return AST::Binary(op, left, right, pos)
+      .set(AST::Type::ASSIGN);
+  }
+
+  AST AST::Prog(
+    const std::vector<AST>& prog,
+    const std::tuple<uint64_t, uint64_t> pos
+  ) {
+
+    return AST()
+      .set(AST::Type::PROG)
+      .set(pos)
+      .set("prog", prog);
+  }
+
+
+} // namespace leor_new
+
+
+namespace leor_old
 {
   BasicAST::BasicAST(
     Type type,
     const std::tuple<uint64_t, uint64_t>& pos
   )
     : type(type), pos(pos)
+  { }
+
+  BoolAST::BoolAST(
+    bool value,
+    const std::tuple<uint64_t, uint64_t>& pos
+  )
+    : BasicAST(Type::BOOL, pos), value(value)
   { }
 
   IntAST::IntAST(
@@ -43,4 +197,55 @@ namespace leor
   )
     : BasicAST(Type::VAR, pos), name(name)
   { }
+
+  FunctionAST::FunctionAST(
+    std::string name,
+    std::vector<std::unique_ptr<BasicAST>>&& args,
+    std::unique_ptr<BasicAST>&& body,
+    const std::tuple<uint64_t, uint64_t>& pos
+  )
+    : BasicAST(Type::FUNCTION, pos), name(name), args(std::move(args)), body(std::move(body))
+  { }
+
+  VariableAST::VariableAST(
+    std::string name,
+    const std::tuple<uint64_t, uint64_t>& pos,
+    bool isMutable
+  )
+    : BasicAST(Type::VARIABLE, pos), name(name), isMutable(isMutable)
+  { }
+
+  BinaryAST::BinaryAST(
+    const std::string op,
+    std::unique_ptr<BasicAST>&& left,
+    std::unique_ptr<BasicAST>&& right,
+    const std::tuple<uint64_t, uint64_t>& pos
+  )
+    : BasicAST(Type::BINARY, pos), op(op), left(std::move(left)), right(std::move(right))
+  { }
+
+  CallAST::CallAST(
+    std::unique_ptr<BasicAST>&& func,
+    std::vector<std::unique_ptr<BasicAST>>&& args,
+    const std::tuple<uint64_t, uint64_t>& pos
+  )
+    : BasicAST(Type::CALL, pos), func(std::move(func)), args(std::move(args))
+  { }
+
+  ProgAST::ProgAST(
+      std::vector<std::unique_ptr<BasicAST>>&& prog,
+      const std::tuple<uint64_t, uint64_t>& pos
+    )
+      : BasicAST(Type::PROG, pos), prog(std::move(prog))
+    { }
+
+  const hash_map<std::string, uint64_t> OP_PRECEDENCE
+  {{
+    {"=",  1},
+    {"||", 2},
+    {"&&", 3},
+    {"<",  7}, {">",  7}, {"<=", 7}, {">=", 7}, {"==", 7}, {"!=", 7},
+    {"+", 10}, {"-", 10},
+    {"*", 20}, {"/", 20}, {"%", 20},
+  }};
 } // namespace leor
--- src/Parser/AST.h
+++ src/Parser/AST.h
@@ -4,10 +4,141 @@
 #define LEOR_AST_H
 
 #include <memory>
+#include <variant>
 
 #include "Lexer/Lexer.h"
 
-namespace leor
+namespace leor_new
+{
+  struct AST
+  {
+  private:
+    AST() = default;
+  
+  public:
+    using Value = std::variant<
+      bool,
+      int64_t,
+      double,
+      std::string,
+      char,
+      AST,
+      std::vector<AST> 
+    >;
+
+    enum class Type
+    {
+      NONE,
+      BOOL, INT, FLOAT, STRING, CHAR, VAR,
+      FUNCTION, VARIABLE, CALL,
+      IF, WHILE, FOR,
+      ASSIGN, BINARY,
+      PROG,
+    };
+
+    Type type;
+    std::unordered_map<std::string, Value> values;
+    std::tuple<uint64_t, uint64_t> pos;
+
+    Value& operator[](const std::string& key)
+    {
+      return values[key];
+    }
+
+    const Value& at(const std::string& key)
+    {
+      return values.at(key);
+    }
+
+  private:
+    AST& set(Type type);
+    AST& set(const std::tuple<uint64_t, uint64_t>& pos);
+    AST& set(const std::string& key, const Value& value);
+    AST& set(const std::string& key, const AST& value);
+
+    // Constructors
+  public:
+    AST None();
+
+    AST Bool(
+      const bool& value,
+      const std::tuple<uint64_t, uint64_t> pos = std::make_tuple(0UL, 0UL)
+    );
+
+    AST Int(
+      const int64_t& value,
+      const std::tuple<uint64_t, uint64_t> pos = std::make_tuple(0UL, 0UL)
+    );
+
+    AST Float(
+      const double& value,
+      const std::tuple<uint64_t, uint64_t> pos = std::make_tuple(0UL, 0UL)
+    );
+
+    AST String(
+      const std::string& value,
+      const std::tuple<uint64_t, uint64_t> pos = std::make_tuple(0UL, 0UL)
+    );
+
+    AST Char(
+      const char& value,
+      const std::tuple<uint64_t, uint64_t> pos = std::make_tuple(0UL, 0UL)
+    );
+
+    AST Var(
+      const std::string& value,
+      const std::tuple<uint64_t, uint64_t> pos = std::make_tuple(0UL, 0UL)
+    );
+
+    AST Function(
+      const std::string& name,
+      const std::vector<AST>& args,
+      const AST& body,
+      const std::tuple<uint64_t, uint64_t> pos = std::make_tuple(0UL, 0UL)
+    );
+
+    AST Variable(
+      const std::string& name,
+      const AST& value,
+      const bool& is_constant,
+      const std::tuple<uint64_t, uint64_t> pos = std::make_tuple(0UL, 0UL)
+    );
+
+    AST Call(
+      AST function,
+      const std::vector<AST>& args,
+      const std::tuple<uint64_t, uint64_t> pos = std::make_tuple(0UL, 0UL)
+    );
+
+    // AST If(); // TODO
+
+    // AST While(); // TODO
+
+    // AST For(); // TODO
+
+    AST Binary(
+      const std::string& op,
+      const AST& left,
+      const AST& right,
+      const std::tuple<uint64_t, uint64_t> pos = std::make_tuple(0UL, 0UL)
+    );
+
+    AST Assign(
+      const std::string& op,
+      const AST& left,
+      const AST& right,
+      const std::tuple<uint64_t, uint64_t> pos = std::make_tuple(0UL, 0UL)
+    );
+
+    AST Prog(
+      const std::vector<AST>& prog,
+      const std::tuple<uint64_t, uint64_t> pos = std::make_tuple(0UL, 0UL)
+    );
+  };
+} // namespace leor_new
+
+
+namespace leor_old
 {
   // struct BasicAST - A basic AST node
   struct BasicAST
@@ -15,9 +146,11 @@ namespace leor
     enum class Type
     {
       NONE,
-      INT, FLOAT, STRING, CHAR, VAR,
-      FUNCTION, VARIABLE, IF, WHILE, FOR,
+      BOOL, INT, FLOAT, STRING, CHAR, VAR,
+      FUNCTION, VARIABLE, CALL,
+      IF, WHILE, FOR,
       ASSIGN, BINARY,
+      PROG,
     };
 
     Type type;
@@ -25,6 +158,16 @@ namespace leor
 
     BasicAST(
       Type type,
+      const std::tuple<uint64_t, uint64_t>& pos = std::make_tuple(0, 0)
+    );
+  };
+
+  struct BoolAST : BasicAST
+  {
+    bool value;
+
+    BoolAST(
+      bool value,
       const std::tuple<uint64_t, uint64_t>& pos = std::make_tuple(0, 0)
     );
   };
@@ -82,11 +225,13 @@ namespace leor
   struct FunctionAST : BasicAST
   {
     std::string name;
-    std::vector<BasicAST> args;
+    std::vector<std::unique_ptr<BasicAST>> args;
     std::unique_ptr<BasicAST> body;
 
     FunctionAST(
       std::string name,
+      std::vector<std::unique_ptr<BasicAST>>&& args,
+      std::unique_ptr<BasicAST>&& body,
       const std::tuple<uint64_t, uint64_t>& pos = std::make_tuple(0, 0)
     );
   };
@@ -103,11 +248,54 @@ namespace leor
       bool isMutable = false
     );
   };
+
+  struct BinaryAST : BasicAST
+  {
+    std::string op;
+    std::unique_ptr<BasicAST> left;
+    std::unique_ptr<BasicAST> right;
+
+    BinaryAST(
+      const std::string op,
+      std::unique_ptr<BasicAST>&& left,
+      std::unique_ptr<BasicAST>&& right,
+      const std::tuple<uint64_t, uint64_t>& pos = std::make_tuple(0, 0)
+    );
+  };
+
+  struct CallAST : BasicAST
+  {
+    std::unique_ptr<BasicAST> func;
+    std::vector<std::unique_ptr<BasicAST>> args;
+
+    CallAST(
+      std::unique_ptr<BasicAST>&& func,
+      std::vector<std::unique_ptr<BasicAST>>&& args,
+      const std::tuple<uint64_t, uint64_t>& pos = std::make_tuple(0, 0)
+    );
+  };
+
+  struct ProgAST : BasicAST
+  {
+    std::vector<std::unique_ptr<BasicAST>> prog;
+
+    ProgAST(
+      std::vector<std::unique_ptr<BasicAST>>&& prog,
+      const std::tuple<uint64_t, uint64_t>& pos = std::make_tuple(0, 0)
+    );
+  };
   
-  // class Parser - A parser for a stream of tokens
+  // Precedence of operators
+  extern const hash_map<std::string, uint64_t> OP_PRECEDENCE;
 
 
 } // namespace leor
 
+
+namespace std
+{
+  // TODO: Implement to_string for BasicAST
+  string to_string(leor::BasicAST* ast);
+} // namespace std
 
 #endif // LEOR_AST_H
